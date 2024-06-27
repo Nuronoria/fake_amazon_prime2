5338422bfd3e72e442195a08a5e286d9fbd08f26
Nuronoria <jrwieloch@gmail.com>
2024-06-27 21:22:46 +0200

    merge?
    hoffentlich klappt es noch

--- login/src/main/java/com/mycompany/login/SessionManager.java
+++ login/src/main/java/com/mycompany/login/SessionManager.java
@@ -11,6 +11,6 @@ package com.mycompany.login;
 public class SessionManager {
     public static int userID;
     public static int movieID;
-    public static boolean admin;
+    public static boolean admin = true;
     
 }
