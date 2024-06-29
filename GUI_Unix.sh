35484478ad34b5105eb82b146d14f66464545e1d
Magna <153079425+Magn4@users.noreply.github.com>
2024-06-29 13:10:47 +0200

    Fixe: Solved the docker run problem

--- GUI_Unix.sh
+++ GUI_Unix.sh
@@ -7,7 +7,7 @@ docker rm userlogin-db-container
 
 docker build -t userlogin-db -f Dockerfile-db .
 
-docker run  --env-file db_secrets.env -d --name userlogin-db-container -p 3308:3308 userlogin-db
+docker run  -d --name userlogin-db-container -p 3308:3308 userlogin-db
 
 cd ../login
 mvn clean package
